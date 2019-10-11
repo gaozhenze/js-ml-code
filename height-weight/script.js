1c1f2e8d042e1e64bfaabb7d3ff03ae9142d1a80
lewis617 <lewis617@163.com>
2019-10-11 20:56:37 +0800

    05-03-训练、预测、反归一化

--- height-weight/script.js
+++ height-weight/script.js
@@ -1,7 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 import * as tfvis from '@tensorflow/tfjs-vis';
 
-window.onload = () => {
+window.onload = async () => {
     const heights = [150, 160, 170];
     const weights = [40, 50, 60];
 
@@ -15,7 +15,21 @@ window.onload = () => {
     );
 
     const inputs = tf.tensor(heights).sub(150).div(20);
-    inputs.print();
     const labels = tf.tensor(weights).sub(40).div(20);
-    labels.print();
+
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    model.compile({ loss: tf.losses.meanSquaredError, optimizer: tf.train.sgd(0.1) });
+
+    await model.fit(inputs, labels, {
+        batchSize: 3,
+        epochs: 200,
+        callbacks: tfvis.show.fitCallbacks(
+            { name: '训练过程' },
+            ['loss']
+        )
+    });
+
+    const output = model.predict(tf.tensor([180]).sub(150).div(20));
+    alert(`如果身高为 180cm，那么预测体重为 ${output.mul(20).add(40).dataSync()[0]}kg`);
 };
