73888bf8dde9de35ba4735fbf48731cfebaf41bc
lewis617 <lewis617@163.com>
2019-10-18 20:54:03 +0800

    09-02-加载带有噪音的二分类数据集

---
+++ overfit/script.js
@@ -0,0 +1,16 @@
+import * as tfvis from '@tensorflow/tfjs-vis';
+import { getData } from './data';
+
+window.onload = () => {
+    const data = getData(200, 3);
+
+    tfvis.render.scatterplot(
+        { name: '训练数据' },
+        {
+            values: [
+                data.filter(p => p.label === 1),
+                data.filter(p => p.label === 0),
+            ]
+        }
+    );
+};
