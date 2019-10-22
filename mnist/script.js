3726d8e5fc36eb277e3a5a4e16013c2c756a44d4
lewis617 <lewis617@163.com>
2019-10-23 00:31:05 +0800

    10-05-进行预测

--- mnist/script.js
+++ mnist/script.js
@@ -76,11 +76,45 @@ window.onload = async () => {
 
     await model.fit(trainXs, trainYs, {
         validationData: [testXs, testYs],
-        epochs: 50,
+        batchSize: 500,
+        epochs: 20,
         callbacks: tfvis.show.fitCallbacks(
             { name: '训练效果' },
             ['loss', 'val_loss', 'acc', 'val_acc'],
             { callbacks: ['onEpochEnd'] }
         )
     });
+
+    const canvas = document.querySelector('canvas');
+
+    canvas.addEventListener('mousemove', (e) => {
+        if (e.buttons === 1) {
+            const ctx = canvas.getContext('2d');
+            ctx.fillStyle = 'rgb(255,255,255)';
+            ctx.fillRect(e.offsetX, e.offsetY, 25, 25);
+        }
+    });
+
+    window.clear = () => {
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'rgb(0,0,0)';
+        ctx.fillRect(0, 0, 300, 300);
+    };
+
+    clear();
+
+    window.predict = () => {
+        const input = tf.tidy(() => {
+            return tf.image.resizeBilinear(
+                tf.browser.fromPixels(canvas),
+                [28, 28],
+                true
+            ).slice([0, 0, 0], [28, 28, 1])
+            .toFloat()
+            .div(255)
+            .reshape([1, 28, 28, 1]);
+        });
+        const pred = model.predict(input).argMax(1);
+        alert(`预测结果为 ${pred.dataSync()[0]}`);
+    };
 };
