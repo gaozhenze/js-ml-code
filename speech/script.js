31dac116d7191da68841ee0bc616e99e834bf532
lewis617 <lewis617@163.com>
2019-10-30 21:46:49 +0800

    13-03-进行语音识别

--- speech/script.js
+++ speech/script.js
@@ -12,6 +12,20 @@ window.onload = async () => {
 
     await recognizer.ensureModelLoaded();
 
-    const labels = recognizer.wordLabels();
-    console.log(labels);
+    const labels = recognizer.wordLabels().slice(2);
+    const resultEl = document.querySelector('#result');
+    resultEl.innerHTML = labels.map(l => `
+        <div>${l}</div>
+    `).join('');
+    recognizer.listen(result => {
+        const { scores } = result;
+        const maxValue = Math.max(...scores);
+        const index = scores.indexOf(maxValue) - 2;
+        resultEl.innerHTML = labels.map((l, i) => `
+        <div style="background: ${i === index && 'green'}">${l}</div>
+        `).join('');
+    }, {
+        overlapFactor: 0.3,
+        probabilityThreshold: 0.9
+    });
 };
