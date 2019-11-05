c2158cd868767dfb0b45234d53f47441deddf722
lewis617 <lewis617@163.com>
2019-11-05 22:23:58 +0800

    14-03-语音识别迁移学习的训练和预测

--- speech-cn/script.js
+++ speech-cn/script.js
@@ -1,4 +1,5 @@
 import * as speechCommands from '@tensorflow-models/speech-commands';
+import * as tfvis from '@tensorflow/tfjs-vis';
 
 const MODEL_PATH = 'http://127.0.0.1:8080';
 let transferRecognizer;
@@ -22,4 +23,31 @@ window.collect = async (btn) => {
     );
     btn.disabled = false;
     document.querySelector('#count').innerHTML = JSON.stringify(transferRecognizer.countExamples(), null, 2);
+};
+
+window.train = async () => {
+    await transferRecognizer.train({
+        epochs: 30,
+        callback: tfvis.show.fitCallbacks(
+            { name: '训练效果' },
+            ['loss', 'acc'],
+            { callbacks: ['onEpochEnd'] }
+        )
+    });
+};
+
+window.toggle = async (checked) => {
+    if (checked) {
+        await transferRecognizer.listen(result => {
+            const { scores } = result;
+            const labels = transferRecognizer.wordLabels();
+            const index = scores.indexOf(Math.max(...scores));
+            console.log(labels[index]);
+        }, {
+            overlapFactor: 0,
+            probabilityThreshold: 0.75
+        });
+    } else {
+        transferRecognizer.stopListening();
+    }
 };
