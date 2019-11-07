1ba1c72de1fded4df5cb0b5d5b650502d9faab19
lewis617 <lewis617@163.com>
2019-11-07 20:04:46 +0800

    14-05-声控轮播图

--- slider/script.js
+++ slider/script.js
@@ -2,6 +2,7 @@ import * as speechCommands from '@tensorflow-models/speech-commands';
 
 const MODEL_PATH = 'http://127.0.0.1:8080';
 let transferRecognizer;
+let curIndex = 0;
 
 window.onload = async () => {
     const recognizer = speechCommands.create(
@@ -25,12 +26,25 @@ window.toggle = async (checked) => {
             const { scores } = result;
             const labels = transferRecognizer.wordLabels();
             const index = scores.indexOf(Math.max(...scores));
-            console.log(labels[index]);
+            window.play(labels[index]);
         }, {
             overlapFactor: 0,
-            probabilityThreshold: 0.75
+            probabilityThreshold: 0.5
         });
     } else {
         transferRecognizer.stopListening();
     }
+};
+
+window.play = (label) => {
+    const div = document.querySelector('.slider>div');
+    if (label === '上一张') {
+        if (curIndex === 0) { return; }
+        curIndex -= 1;
+    } else {
+        if (curIndex === document.querySelectorAll('img').length - 1) { return; }
+        curIndex += 1;
+    }
+    div.style.transition = "transform 1s"
+    div.style.transform = `translateX(-${100 * curIndex}%)`;
 };
